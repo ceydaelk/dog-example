2bd43d890c6775606e73d7925e0d7aa07946386c
ceydaelk <ceydaelk@gmail.com>
2025-04-13 12:52:42 +0300

    App.jsx değiştirildi

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  //array donuyorsa boş array atıyorum
+  //object dönüyorsa null
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+
+  const API_URL = "https://dog.ceo/api/breed/chihuahua/images";
+  fetch(API_URL)
+  .then((res) => res.json())
+  .then((veri) => setImages(veri.message));
+    
+  }, []);
+
+  return (
+    <div>
+      {images.map((imageUrl) => (
+        <img src={imageUrl} width={100} height={100} />
+      ))}
+    </div>
+  );
+
+}
+
+export default App
